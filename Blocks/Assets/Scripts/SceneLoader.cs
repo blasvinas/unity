7ec2b029021b76065e3e522b7f0c2b371285db6c
Blas Vinas <blasvinas@gmail.com>
2020-08-09 17:31:23 -0400

    add score

--- Blocks/Assets/Scripts/SceneLoader.cs
+++ Blocks/Assets/Scripts/SceneLoader.cs
@@ -5,15 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadStartScene()
-    {
-        SceneManager.LoadScene(0);
-    }
 
     public void LoadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Level 1");
+        FindObjectOfType<Game>().ResetGame();
+    }
+
 
     public void QuitGame()
     {
